9769ba82bf0103c67e0fa024f507a6717c9804e7
johnmart19 <johnivan19999@gmail.com>
2022-11-10 13:59:49 +0200

    munch: Import Xiaomi Vibrator
    - This commit reverts branch of commits according to Vibrator changes
    0779e4cf10e887abb2ffbf5b1ac777362937ca7a
    6d73cb4b0073053cc06bf7383bf9dbf7d35d083d
    62323bdf4bdb8c1e49efc9b282f8961a2a24ebb5
    050534b56c1ea4924960c760ce92d8d22bb45beb
    ee3f90272379226ad5513f62c0d7c5f1d5fc936b
    811d062376604f5b5ff5fc28a3e9f5b1d0fda5fd
    1e89b0a3ef2cb83ca4cc4dc394f7c4ae92b65ba9
    10bd45818942ed1510ca50883652bfe8ca3b1d87
    3921eff0513b2c5eb6cd6998cc390b17658e1e42
    e5f7c5e73c023c7aeddb9885474a084395dec0fb
    20967b264daadbf99c516cf1d37cc8447f8aa7a9
    
    munch: Import and adapt Xiaomi Proprietary vibrator hal
    
    This change includes:
    - Sepolicy ports from Miui for Xiaomi Vibrator and VibratorFeature hals
    - Import needed Prebuilts of AIDL service and required deps
    - Copy vndk 32 libutils and patch service with it to fix bootloop issues
    
    This change requires:
    - Import Stock vibator hal effects from Miui vendor in device specific trees
    
    munch: Sepolicy: Address additional VibratorFeature nodes
    
    - Vibratorfeature and Vibrator service having access to leds sysfs, so adress to current active permissions
    - Also provide read permissions
    
    Revert "munch: Import vibrator effects from stock"
    
    This reverts commit da9f7b9e662638d0114bda2ae08238d7054517cb.
    
    Munch: Import Haptics props from Miui Munch
    
    Munch: Import all vendor/firmware Xiaomi Vibrator prebuilts
    
    - This are effects which are loaded and used by Xiaomi Vibrator hal + Richtap and VibratorFeature
    
    munch: Sync vibrator libs from miui14
    
    m

--- extract-files.sh
+++ extract-files.sh
@@ -67,6 +67,10 @@ function blob_fixup() {
         vendor/lib64/libdlbdsservice.so | vendor/lib64/soundfx/libhwdap.so)
             "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
             ;;
+        vendor/bin/hw/vendor.xiaomi.hardware.vibratorfeature.service)
+            "${PATCHELF}" --replace-needed "android.hardware.vibrator-V1-ndk_platform.so" "android.hardware.vibrator-V1-ndk.so" "${2}"
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
+            ;;
     esac
 }
 
