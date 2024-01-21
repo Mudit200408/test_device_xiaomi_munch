652ed00b4afb37bcc6ec7a89c7e6f58bdd6e89ee
Quick ⚡ <155861545+username0136@users.noreply.github.com>
2024-01-21 20:51:53 +0530

    sm8250-common: include: local device framework matrix

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -103,6 +103,7 @@ endif
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/framework_compatibility_matrix.xml \
+    $(COMMON_PATH)/device_framework_matrix.xml \
     vendor/aosp/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
